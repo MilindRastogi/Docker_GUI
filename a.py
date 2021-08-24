69f6412f0e9250db0bf645be95794b59cc4f36c5
MilindRastogi <43311538+MilindRastogi@users.noreply.github.com>
2021-08-24 12:02:30 +0530

    Update a.py

--- a.py
+++ a.py
@@ -1,1 +1,13 @@
 
+#!/usr/bin/python3
+
+print("content-type: text/html")
+print("Access-Control-Allow-Origin: *")
+print()
+
+import subprocess
+import cgi
+form = cgi.FieldStorage()
+cmd = form.getvalue("x")
+x = subprocess.getoutput(cmd)
+print(x)
